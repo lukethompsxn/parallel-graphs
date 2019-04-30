4a1393f68c9e379d20b2373859970d70d7b15400
lukethompson <lukethompsxn@gmail.com>
2019-05-01 09:15:02 +1200

    update graph parsing to have -1 as no edge

--- src/algorithms/prims/PrimsSequential.jl
+++ src/algorithms/prims/PrimsSequential.jl
@@ -1,3 +1,4 @@
 include("../../util/Common.jl")
 
-println(parsegraph("/Users/lukethompson/dev/uni/751-Project/res/graphs/random-1.dot"))
+graph = parsegraph("/Users/lukethompson/dev/uni/751-Project/res/graphs/random-1.dot")
+println(graph)
--- src/util/Common.jl
+++ src/util/Common.jl
@@ -3,7 +3,7 @@ function parsegraph(path::String)
 
     open(path) do file
         count = length(collect(eachmatch(r"[0-9]{1,};", read(file, String))))
-        matrix = zeros(count, count)
+        matrix = fill(-1, count, count)
     end
 
     open(path) do file
@@ -11,9 +11,8 @@ function parsegraph(path::String)
             if (occursin(" -> ", line))
                 m = eachmatch(r"[0-9]{1,}", line)
                 vals = collect(m)
-                println(parse(Int64, vals[1].match))
                 if (length(vals) == 3)
-                    matrix[parse(Int64, vals[1].match), parse(Int64, vals[2].match)] = parse(Int64, vals[3].match)
+                    matrix[parse(Int8, vals[1].match), parse(Int8, vals[2].match)] = parse(Int8, vals[3].match)
                 end
             end
         end
