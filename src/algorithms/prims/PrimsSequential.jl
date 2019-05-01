ef1265e4a929030694320c12c30c29070551bb21
lukethompson <lukethompsxn@gmail.com>
2019-05-02 09:44:03 +1200

    implement sequential prims algorithm

--- src/algorithms/prims/PrimsSequential.jl
+++ src/algorithms/prims/PrimsSequential.jl
@@ -1,4 +1,37 @@
 include("../../util/Common.jl")
 
-graph = parsegraph("/Users/lukethompson/dev/uni/751-Project/res/graphs/random-1.dot")
-println(graph)
+graph = parsegraph("/Users/lukethompson/dev/uni/751-Project/res/graphs/random-1.dot", "graph")
+
+len = 0
+if (length(graph) > 0)
+    len = length(graph[1, :])
+end
+nodes = Set(1:len)
+
+mstnodes = Set()
+mst = fill(-1, len, len)
+
+push!(mstnodes, pop!(nodes))
+while length(mstnodes) != len
+    index = CartesianIndex(first(mstnodes), 1)
+    val = graph[first(mstnodes), 1] #it would be much faster to just add them to a queue
+
+    for node in mstnodes
+        if minimum(graph[node, :]) < val
+            index = CartesianIndex(node, argmin(graph[node, :]))
+            val = minimum(graph[node, :])
+        end
+    end
+
+    if (!in(index[2], mstnodes))
+        push!(mstnodes, index[2])
+        mst[index] = val
+        mst[index[2], index[1]] = val
+    end
+    graph[index] = typemax(Int16)
+end
+
+writegraph(mst, "minimum spanning tree (prims)")
+println(mst)
+
+
--- src/util/Common.jl
+++ src/util/Common.jl
@@ -1,9 +1,10 @@
-function parsegraph(path::String)
+function parsegraph(path::String, type::String)
+    integer = Int16
     matrix = []
 
     open(path) do file
         count = length(collect(eachmatch(r"[0-9]{1,};", read(file, String))))
-        matrix = fill(-1, count, count)
+        matrix = fill(typemax(integer), count, count)
     end
 
     open(path) do file
@@ -12,10 +13,18 @@ function parsegraph(path::String)
                 m = eachmatch(r"[0-9]{1,}", line)
                 vals = collect(m)
                 if (length(vals) == 3)
-                    matrix[parse(Int8, vals[1].match), parse(Int8, vals[2].match)] = parse(Int8, vals[3].match)
+                    matrix[parse(integer, vals[1].match), parse(integer, vals[2].match)] = parse(integer, vals[3].match)
+                    if (type == "graph")
+                        matrix[parse(integer, vals[2].match), parse(integer, vals[1].match)] = parse(integer, vals[3].match)
+                    end
                 end
             end
         end
         return matrix
     end
 end
+
+function writegraph(graph::Array, type::String)
+end
+
+
