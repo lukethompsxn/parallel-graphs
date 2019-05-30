9f3420d63cd77f644bd1b1c351aa9363cf28aa51
lukethompson <lukethompsxn@gmail.com>
2019-05-30 16:51:11 +1200

    update LightGraphs benchmarking

--- test/LightGraphsBenchmarking.jl
+++ test/LightGraphsBenchmarking.jl
@@ -1,13 +1,31 @@
 include("../src/util/Common.jl")
 using LightGraphs
+using BenchmarkTools
+using Statistics
+using Dates
 import LightGraphs.Parallel
 
-function floyd()
-    graph = SimpleDiGraph(parsefloyd("./res/generated/[digraph]-random-1000.dot"))
-    print("Sequential")
-    @time @sync LightGraphs.floyd_warshall_shortest_paths(graph)
-    print("Parallel")
-    @time @sync Parallel.floyd_warshall_shortest_paths(graph)
+function printresults(trial)
+    dump(trial)
+
+    println("min: ", minimum(trial))
+    println("median: ", median(trial))
+    println("mean: ", mean(trial))
+    println("max: ", maximum(trial))
+
+    println("total seconds: ", sum(trial.times) / 1e9)
+    println("total samples: ", length(trial.times))
 end
 
-floyd()
+graph = SimpleDiGraph(parsefloyd("./res/generated/[digraph]-random-1000.dot"))
+
+g1 = @sync copy(graph)
+g2 = @sync copy(graph)
+
+seq = @benchmark LightGraphs.floyd_warshall_shortest_paths(g1) samples=50 seconds=120 evals=1
+par = @benchmark Parallel.floyd_warshall_shortest_paths(g2) samples=50 seconds=120 evals=1
+
+println("Sequential Benchmarking...")
+printresults(seq)
+println("Parallel Benchmarking...")
+printresults(par)
