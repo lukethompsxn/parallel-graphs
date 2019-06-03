cc282612bebe5b13eea5138e82f59c35c75235a5
Z-Qi <g.qiao97@gmail.com>
2019-06-03 15:44:27 +1200

    updated run script

--- run.jl
+++ run.jl
@@ -1,27 +1,62 @@
-using BenchmarkTools
-primsPrl = "src/algorithms/prims/PrimsParallel.jl"
-primsSeq = "src/algorithms/prims/vector/PrimsSequential.jl"
+include("src/util/Common.jl")
 
-dijksPrl = "src/algorithms/dijkstra/DijkstraParallel.jl"
-dijksSeq = "src/algorithms/dijkstra/DijkstraSequential.jl"
+include("src/algorithms/prims/vector/PrimsSequential.jl")
+include("src/algorithms/prims/vector/PrimsParallel.jl")
+include("src/algorithms/prims/vector/PrimsParallelNodes.jl")
+include("src/algorithms/prims/vector/PrimsSequentialNodes.jl")
+include("src/algorithms/floyd-warshall/FWSequential.jl")
+include("src/algorithms/floyd-warshall/parallel/FWParallelThreads.jl")
+include("src/algorithms/floyd-warshall/parallel/FWParallelDistributed.jl")
 
-algorithm = ""
-file = "res/graphs/generated/[graph]-random-1000.dot"
+if length(ARGS) < 1
+    println("Please specify graph to run algorithms on")
+    exit()
+end
 
-if isdefined(ARGS, 3) file = ARGS[3] end
+graphName = ARGS[1]
+if !isfile(graphName)
+    println("File \"$graphName\" does not exist, please specify another graph file")
+    exit()
+end
 
-if ARGS[1] == "prims"
-    if ARGS[2] == "sequential"
-        algorithm = primsSeq
-    elseif ARGS[2] == "parallel"
-        algorithm = primsPrl
-    end
-elseif ARGS[1] == "dijkstra"
-    if ARGS[2] == "sequential"
-        algorithm = dijksSeq
-    elseif ARGS[2] == "parallel"
-        algorithm = dijksPrl
+if length(ARGS) > 1
+    algorithm = ARGS[2]
+    type = length(ARGS) > 2 ? ARGS[3] : nothing
+
+    # assigns various settings or their defaults
+    parallel = "-p" in ARGS ? ARGS[findfirst(d -> d == "-p", ARGS) + 1] == "true" : false
+
+    if algorithm == "prims"
+        graph = parseprims(graphName)
+        if type == "nodes"
+            if parallel
+                output = vector_prims_parallel_nodes(copy(graph))
+            else
+                output = vector_prims_sequential_nodes(copy(graph))
+            end
+        else
+            if parallel
+                output = vector_prims_parallel(copy(graph))
+            else
+                output = vector_prims_sequential(copy(graph))
+            end
+        end
+    elseif algorithm == "floyds"
+        graph = parsefloyd(graphName)
+        if parallel
+            output = fwp(copy(graph))
+        else
+            output = fws(copy(graph))
+        end
+    else
+        println("Please specify algorithm\n",
+        "\tprims - minimum spanning tree\n",
+        "\tfloyds - floyd-warshall all (shortest) path costs")
+        exit()
     end
 end
 
-run(`julia $algorithm $file`)
+type = graphName[findfirst("[", graphName)[1] + 1 : findfirst("]", graphName)[1] - 1]
+name = graphName[findfirst("-", graphName)[1] + 1 : findfirst(".", graphName)[1] - 1]
+
+writegraph(output, type, name)
